13533bff8ca306eb87b6c2d69400a79f6ee290f4
Juchan Park <pjc6269@gmail.com>
2024-05-17 17:41:28 +0900

    월별기준금리

---
+++ 기준금리 월별.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 15:30:45 2024
+
+@author: jcp
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Read the CSV data into a DataFrame
+data = pd.read_csv('한국은행 기준금리 및 여수신금리_17153127.csv', header=0, parse_dates=True)
+data.columns = ['날짜', '기준금리']
+# data.set_index('날짜')
+# %%
+# Extract the base rate values
+dates = data['날짜']
+values = data['기준금리']
+
+# %%
+# 문자열 날짜를 datetime 객체로 변환
+import matplotlib.dates as mdates
+from datetime import datetime
+
+dates = [datetime.strptime(date, "%Y/%m") for date in dates]
+
+# %%
+
+# 폰트 경로
+from add_font import *
+
+reg()
+
+# 폰트 설정
+plt.rc('font', family='NaNumBarunGothic')
+plt.rcParams['figure.dpi'] = 140
+# %%
+# 선 그래프 그리기
+plt.figure(figsize=(16, 6))
+
+# fig, ax = plt.subplots()
+plt.plot(dates, values)
+
+# x축 범위 설정
+plt.xlim(dates[0], dates[-1])
+
+# x축 눈금을 3개월
+plt.xticks(dates[::3], [date.strftime("%y/%m") for date in dates[::3]], rotation=45, ha='right', fontsize=8)
+
+# x축 눈금을 45도 회전
+plt.xticks(rotation=45)
+
+plt.title('한국 기준금리 월별 변화 (2013년 4월 ~ 2024년 5월)')
+plt.xlabel('날짜')
+plt.ylabel('기준금리')
+
+plt.show()
+
+
